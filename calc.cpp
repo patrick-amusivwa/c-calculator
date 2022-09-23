5e55281ac2c5f4c21c80d3343cfef751d5c869bb
Patrick amusivwa <patrick.amusivwa@students.jkuat.ac.ke>
2022-09-23 17:29:52 +0300

    formating

--- calc.cpp
+++ calc.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int num1,num2;
+int main()
+{
+    int num1, num2;
     cout << "Enter first number: ";
     cin >> num1;
     cout << "Enter second number: ";
     cin >> num2;
-    cout << "Sum is: " << num1+num2 << endl;
+    cout << "Sum is: " << num1 + num2 << endl;
 
     return 0;
-
 }
