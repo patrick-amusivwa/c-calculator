f0b8f7628fa024149a3f8e2e2bfbcf06b591a727
Patrick amusivwa <patrick.amusivwa@students.jkuat.ac.ke>
2022-09-23 17:27:48 +0300

    basic functionallity

--- calc.cpp
+++ calc.cpp
@@ -3,6 +3,11 @@ using namespace std;
 int main() {
     int num1,num2;
     cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    cout << "Sum is: " << num1+num2 << endl;
 
     return 0;
+
 }
