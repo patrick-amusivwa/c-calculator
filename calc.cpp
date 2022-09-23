a93f31c2465e7ca01cb55bce1bce399a4be4e5b5
Patrick amusivwa <patrick.amusivwa@students.jkuat.ac.ke>
2022-09-23 18:55:16 +0300

    added operations and error handling

--- calc.cpp
+++ calc.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 using namespace std;
+int calc(int a , char c,int b){
+    double result;
+    if (c=='+'){
+        result=a+b;
+    }
+    else if (c=='-'){
+        result=a-b;
+    }
+    else if (c=='*'){
+        result=a*b;
+    }
+    else if (c=='/'){
+        result=a/b;
+    }
+    else{
+        cout<<"Invalid operator";
+    }
+
+}
 int main()
 {
     int num1, num2;
+    char op;
+
     cout << "Enter first number: ";
     cin >> num1;
+    cout << "Enter operator: ";
+    cin>>op;
     cout << "Enter second number: ";
     cin >> num2;
-    cout << "Sum is: " << num1 + num2 << endl;
+    cout << "result  is: " << calc(num1,op,num2 )<< endl;
 
     return 0;
 }
